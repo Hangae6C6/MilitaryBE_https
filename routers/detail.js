ffb4f33adc103d48e88c221fbe1bd9347888bc03
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-28 16:59:39 +0900

    주석추가

--- routers/detail.js
+++ routers/detail.js
@@ -12,6 +12,7 @@ const {
 const authMiddleware = require("../middleware/authMiddleWare");
 
 //디테일페이지 가져오기 기능
+//썬더클라이언트 테스트 완료(정대규)
 router.get("/challengeDetail", authMiddleware, detailPage);
 
 //하나의 챌린지에 누가 참여하고있고 참여한 유저의 챌린지 진행현황 확인할수있는 기능
@@ -31,9 +32,11 @@ router.get('/challengeJoinBychallengeNum',authMiddleware, detailJoinList_challen
 router.delete('/challengeout',authMiddleware, detailJoinout);
 
 //첼린지상세페이지 step 
+//썬더클라이언트 테스트 완료(정대규)
 router.post('/challengeStep',authMiddleware, detailSteps);
 
 //챌린지 Ranking
+//썬더클라이언트 테스트 완료(황인호)
 router.get('/challengeRanking',authMiddleware,challengeRank)
 
 module.exports = router;
