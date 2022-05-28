10a1f15f94438e02eae9fd5f2fa70360ea6a00c0
eorb1320 <eorb910623@gmail.com>
2022-05-28 15:22:54 +0900

    수정
    수정

--- app.js
+++ app.js
@@ -35,14 +35,14 @@ sequelize
   });
 
 //인증서 불러오기
-const privateKey = fs.readFileSync(__dirname + "/pizzaboy_shop.key", "utf8");
-const certificate = fs.readFileSync(__dirname + "/pizzaboy_shop__crt.pem", "utf8");
-const ca = fs.readFileSync(__dirname + "/pizzaboy_shop__ca.pem", "utf8");
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
+// const privateKey = fs.readFileSync(__dirname + "/pizzaboy_shop.key", "utf8");
+// const certificate = fs.readFileSync(__dirname + "/pizzaboy_shop__crt.pem", "utf8");
+// const ca = fs.readFileSync(__dirname + "/pizzaboy_shop__ca.pem", "utf8");
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca,
+// };
 
 //https 미들웨어 정의
 
@@ -73,15 +73,15 @@ const requestMiddleware = (req, res, next) => {
 
 //https 리다이렉션
 //app_low : http 전용 미들웨어
-app_low.use((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    const to = `https://${req.hostname}:${httpPort}${req.url}`;
-    console.log(to);
-    res.redirect(to);
-  }
-});
+// app_low.use((req, res, next) => {
+//   if (req.secure) {
+//     next();
+//   } else {
+//     const to = `https://${req.hostname}:${httpPort}${req.url}`;
+//     console.log(to);
+//     res.redirect(to);
+//   }
+// });
 
 const io = new Server(server, {
   cors: {
@@ -165,14 +165,14 @@ app.use(function (err, req, res, next) {
   res.status(500).send("Something Broke!");
 });
 
-// app.listen(3001, () => {
-//   console.log(3001, "번으로 서버가 켜졌어요!");
+app.listen(3000, () => {
+  console.log(3000, "번으로 서버가 켜졌어요!");
+});
+
+// http.createServer(app_low).listen(httpPort, () => {
+//   console.log("http 서버가 켜졌어요");
 // });
 
-http.createServer(app_low).listen(httpPort, () => {
-  console.log("http 서버가 켜졌어요");
-});
-
-https.createServer(credentials, app).listen(httpsPort, () => {
-  console.log("https 서버가 켜졌어요");
-});
+// https.createServer(credentials, app).listen(httpsPort, () => {
+//   console.log("https 서버가 켜졌어요");
+// });
