0533d0932899b0755638184aaa911607ae6db52c
jason taekyun Kim <saurus234@gmail.com>
2022-05-29 22:32:23 +0900

    update code

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": "admin",
-    "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "soliderchallengers",
-    "host": "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+  development: {
+    username: "admin",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "soliderchallengers",
+    host: "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "admin",
-    "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "soliderchallengers",
-    "host": "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+  test: {
+    username: "admin",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "soliderchallengers",
+    host: "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "admin",
-    "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "soliderchallengers",
-    "host": "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "admin",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "soliderchallengers",
+    host: "soliderchallengers.chitz0qkrhbx.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
+  },
+};
