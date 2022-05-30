c13edf8cdd8f96aa987d043f8e0068d1b94f1cef
eorb1320 <eorb910623@gmail.com>
2022-05-30 10:16:15 +0900

    수정
    수정

--- migrations/20220530005853-create-user-data.js
+++ migrations/20220530005853-create-user-data.js
@@ -1,34 +1,43 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class UserChallenge extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  UserChallenge.init({
-    userId: {
-      type:DataTypes.STRING,
-      primaryKey:true,
-    },
-    userchallenegNum:DataTypes.INTEGER,
-    challenegNum:DataTypes.STRING,
-    challengeEndDate: DataTypes.STRING,
-    challengeProgress: DataTypes.STRING,
-    challengeTitle: DataTypes.STRING,
-    challengeType: DataTypes.STRING,
-    steps: DataTypes.JSON,
-
-  }, {
-    sequelize,
-    modelName: 'UserChallenge',
-  });
-  return UserChallenge;
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("UserDatas", {
+      startDate: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      endDate: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      armyCategory: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      rank: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      testResult: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        primaryKey: true,
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("UserDatas");
+  },
 };
