2b005d38683127dd213f9fc19fef44b55129c44a
eorb1320 <eorb910623@gmail.com>
2022-05-30 09:53:07 +0900

    migrateions수정
    수정

--- migrations/20220503045257-create-user-data.js
+++ migrations/20220503045257-create-user-data.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("UserData", {
+    await queryInterface.createTable("UserDatas", {
       startDate: {
         allowNull: true,
         type: Sequelize.STRING,
@@ -38,6 +38,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("UserData");
+    await queryInterface.dropTable("UserDatas");
   },
 };
--- migrations/20220524152332-create-test.js
+++ migrations/20220524152332-create-test.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Test', {
+    await queryInterface.createTable('Tests', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Test');
+    await queryInterface.dropTable('Tests');
   }
 };
